06732445caf2a57b39b86e8c12784832ac743498
wonho-jung <wonho.jung92@gmail.com>
2020-12-27 13:39:26 -0500

    nav and homepage

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC51yBTcs8cv-YswMD-9uI-xE0_JvOFLBU",
+  authDomain: "v2-cb8fa.firebaseapp.com",
+  projectId: "v2-cb8fa",
+  storageBucket: "v2-cb8fa.appspot.com",
+  messagingSenderId: "238512816958",
+  appId: "1:238512816958:web:7db6f96498b6c31205626f",
+  measurementId: "G-004Q7QQP01",
+};
