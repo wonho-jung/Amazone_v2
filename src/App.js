a9408a33edd082b4c68313c80f31cacce96fcc2d
wonho-jung <wonho.jung92@gmail.com>
2020-12-30 14:39:59 -0500

    logInForm

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Checkout from "./components/Checkout/Checkout";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
           <Route path="/login">
-            <h1>Login</h1>
+            <Login />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
+
             <Home />
           </Route>
         </Switch>
