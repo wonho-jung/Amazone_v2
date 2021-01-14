562555f384b99da4237f0c028f4eb42e95bdfdaf
wonho-jung <wonho.jung92@gmail.com>
2021-01-14 16:34:42 -0500

    axios with payment

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ import Login from "./components/Login/Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Payment from "./components/Payment/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
 function App() {
   const [{}, dispatch] = useStateValue();
-
+  const promise = loadStripe(
+    "pk_test_51I9cdUFD7T9j3GXeUSZ1zjZI0iSyuaKV1icQQEu7f5pdIEuuZRIRLTDk07NSKmjEzw0N5KqUM6Tby7ZdKiIau67d00x2Zz2Fuj"
+  );
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       console.log("user >>>", authUser);
@@ -40,7 +45,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
 
           <Route path="/">
--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStateValue } from "../../StateProvider";
 import CheckoutProduct from "../Checkout/CheckoutProduct/CheckoutProduct";
 import { Link } from "react-router-dom";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "../../reducer";
 
 import "./Payment.css";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
 
+  useEffect(() => {
+    const getClientSecret = async () => {
+      const res = await axios;
+    };
+  }, [basket]);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe;
+  };
+  const handleChange = (e) => {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
   return (
     <div className="payment">
       <div className="payment__container">
@@ -43,6 +69,25 @@ function Payment() {
         <div className="payment__section">
           <div className="payment__title">
             <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
+              {error && <div>{error}</div>}
+            </form>
           </div>
         </div>
       </div>
